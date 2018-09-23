1c9b58d91b3b2a3f9146f1db7085d29f1d789020
Evan Crane <ecrane@augusta.edu>
2018-09-23 13:04:30 -0400

    added file to hold dfa specific classes

---
+++ src/dfa.py
@@ -0,0 +1,9 @@
+class Dfa:
+    def __init__(self, states, alpha, transfunc, start, final):
+        self.states = states
+        self.alpha = alpha
+        self.transfunc = transfunc
+        self.start = start
+        self.final = final
+    def defineDfa(self):
+        print(vdir(self))
