470d8c869daa7e50d2655c49383e0a578017f6e7
Evan Crane <ecrane@augusta.edu>
2018-10-10 16:06:17 -0400

    Added classes for minimization object binding

--- src/dfa.py
+++ src/dfa.py
@@ -5,8 +5,33 @@ class Dfa:
         self.transfunc = transfunc
         self.start = start
         self.final = final
+
     def printDfa(self):
         print(vdir(self))
+
+
+class State:
+    def __init__(self, name, is_start, is_final, transitions):
+        self.name = name
+        self.is_start = is_start
+        self.is_final = is_final
+        self.transitions = transitions
+
+    def printState(self):
+        print(vdir(self))
+
+
+class MinSet:
+    def __init__(self, designation, number, states, has_start, has_final):
+        self.designation = designation
+        self.number = number
+        self.has_start = has_start
+        self.has_final = has_final
+        self.states = states
+
+    def printMinSet(self):
+        print(vdir(self))
+
 
 def vdir(obj):
     return [x for x in dir(obj) if not x.startswith('__') and not x.startswith('print')]
