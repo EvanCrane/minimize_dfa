4e60f3f8394de8b76c8754b95ee3b49b419fa2ac
Evan Crane <evanf.crane@gmail.com>
2018-10-12 13:47:44 -0400

    Fixing/streamlining loops and adjusting minimize flow

--- src/minimize.py
+++ src/minimize.py
@@ -1,83 +1,135 @@
+#minimize.py
 import src
+from src.error import print_error
 from src.dfa import Dfa
 
 def minimize_main(dfa):
-    state_objs = dfa.to_state_objects
-    distinct =  [[0 for x in range(len(state_objs))] for y in range(len(state_objs))]
-    new_distinct = zero_equiv(state_objs, distinct)
-    finished_distinct = n_equiv(new_distinct, None, state_objs)
-    
-    return dfa_min 
+    states = dfa.to_state_objects()
+    distinct = initialize_table(len(states))
+    if distinct is None:
+        return None
+    print_table(distinct)
+    new_distinct = zero_equiv(distinct, states)
+    if new_distinct is None:
+        return None
+    print_table(new_distinct)
+    finished_distinct = n_equiv(new_distinct, dfa.alpha, states)
+    if finished_distinct is None:
+        return None
+    print_table(finished_distinct)
+    #states_min = 
+    #dfa_min = 
+    #return dfa_min 
 
-def zero_equiv(states_objs, distinct):
-    finals = [f for f in states_objs
+def initialize_table(length):
+    #Initialize 2d array entirely with X's initially
+    if length == 1:
+        table = ["X"]
+        return table
+    elif length > 1:
+        table =  [['X' for x in range(length)] for y in range(length)]
+        completed_list = []
+        for col in table:
+            for row in table:
+                if col == row:
+                    break
+                complete = False
+                for tup in completed_list:
+                    if col in tup and row in tup:
+                        complete = True
+                        break
+                if complete is True:
+                    break
+                table[row][col] = 0
+                completed_list.append((col,row))
+        return table
+    else:
+        print_error("MINIMIZE", "init_table",
+            "The length of state objects is not valid.")
+        return None
+
+def zero_equiv(distinct, states):
+    finals = [f for f in states
                 if f.is_final is True]
-    non_finals = [n for n in states_objs
-                    if n.is_final is false]
-    final_indexes = [i for i in finals.position]
-    non_final_indexes = [i for i in non_finals.position]
-    return set_the_table(distinct, final_indexes, non_final_indexes)
+    non_finals = [n for n in states
+                    if n.is_final is False]
+    final_indexes = []
+    non_final_indexes = []
+    for f in finals:
+        final_indexes.append(f.position)
+    for n in non_finals:
+        non_final_indexes.append(n.position)
+    return set_finals(distinct, final_indexes, non_final_indexes)
     
-def set_the_table(distinct, final_indexes, non_final_indexes):
-    completed_list = []
-    for x in final_indexes:
-        for y in non_final_indexes:
+def set_finals(distinct, final_indexes, non_final_indexes):
+    completed_list = []    
+    for f in final_indexes:
+        for n in non_final_indexes:
+            if distinct[f][n] == 'X':
+                break
             complete = False
             for tup in completed_list:
-                if x in tup and y in tup:
+                if f in tup and n in tup:
                     complete = True
                     break
             if complete is True:
                 break
-            distinct[x][y] = 1
+            distinct[f][n] = 1
+            completed_list.append(f,n)
     return distinct
 
-def n_equiv(distinct, old_distinct, states):
-    if distinct is None:
-        #Were done here
-        return old_distinct
-    elif old_distinct is None:
+def n_equiv(distinct, alphas, states):
         #do the first time loop
-        new_distinct = update_table(distinct, states)
-        n_equiv(new_distinct, distinct, states)
-    else:
-        #do all the other loops
-        is_changed = compare_table(distinct, old_distinct, states)
-        if  is_changed is True:
-            new_distinct = update_table(distinct, states)
-            n_equiv(new_distinct, distinct, states)
+        a_index = 0
+        new_distinct = update_table(distinct, states, alphas[a_index])
+        a_index += 1
+        return _helper_equiv(new_distinct, a_index)
+        print_error("MINIMIZE", "n_equiv",
+            "Problem with input. Something is None where it shouldn't be and/or vice versa")
+
+    def _helper_equiv(distinct, a_index):
+        nonlocal alphas
+        nonlocal states
+        if a_index is not None:
+            #do all the other loops
+            if a_index < len(alphas):
+                new_distinct = update_table(distinct, states, alphas[a_index])
+                a_index += 1
+                _helper_equiv(new_distinct, a_index)
+            else:
+                _helper_equiv(distinct, None)
         else:
-            n_equiv(None, distinct, states)
+            #Were done here
+            return distinct
             
 
-def update_table(distinct, states):
+def update_table(distinct, states, alpha):
     completed_list = []
-    for x in range(len(distinct)):
-        for y in range(len(distinct[0])):
+    for col in range(len(distinct)):
+        for row in range(len(distinct)):
             complete = False
-            if x == y 
+            if col == row or distinct[row][col] == 'X': 
                 break
-            if distinct[x][y] == 1:
-                completed_list.append((x,y))
+            if distinct[row][col] == 1:
+                completed_list.append((col,row))
                 break
             for tup in completed_list:
-                if x in tup and y in tup:
+                if col in tup and row in tup:
                     complete = True
                     break
             if complete is True:
                 break
-            def _helper_compare(states[x], states[y], distinct[x][y], states)
-            is_distinct = compare_states(distinct[x][y], states[x], states[y])
-            completed_list.append((x,y))
+            is_distinct = compare_states(distinct[row][col], 
+                states.position == col, states.position == row)
+            completed_list.append((col,row))
             if is_distinct:
-                distinct[x][y] = 1
+                distinct[row][col] = 1
             else:
-                distinct[x][y] = 0
+                distinct[row][col] = 0
     return distinct           
 
 def determine_distinct(parent, child, distinct, states):
     is_distinct = True
-    if distinct_value != 1:
         
 
     return is_distinct
@@ -85,13 +137,24 @@ def determine_distinct(parent, child, distinct, states):
 
 def compare_states(distinct_value, parent, child):
     is_distinct = True
-    if distinct_value != 1:
-        _helper_compare
+    p_delta = get_trans_output(parent)
+    q_delta = get_trans_output(child)
+    if distinct_value == 0 and dis:
 
     return is_distinct
 
-def get_trans_output(state)
+def compare2(distinct, p, q):
+    is_distinct = distinct[q][p]
+    if is_distinct != 'X':
+        return is_distinct
+    else:
+        return None
+        print_error("MINIMIZE", "compare2()", 
+            "Problem comparing states. There was a state reflection issue")
+        
 
+def get_trans_output(state):
+    output = []
     return output
     
 def find_similar_states(distinct):
@@ -99,8 +162,13 @@ def find_similar_states(distinct):
 
     return similar_states
 
-
 def compare_table(distinct, old_distinct, states):
     is_changed = True
-
+    if distinct == old_distinct:
+        is_changed = False
     return is_changed
+
+def print_table(distinct):
+    print("EVENT: Printing table...")
+    for row in reversed(distinct):
+        print(row)
