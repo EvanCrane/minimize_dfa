bbb281bfc020c27e6da512b7867ae279c2533712
Evan Crane <ecrane@augusta.edu>
2018-10-13 03:56:27 -0400

    Wrote and testing verification

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+import os
+import sys
+from pathlib import Path
+from os.path import dirname, join
 import src
 from src.parse import parseMain
 from src.parsefile import parse_main
 from src.prettyPrint import pretty_print_main
 from src.verify import verify_dfa_main
 from src.minimize import minimize_main
+from src.error import print_error
 
 
 def main():
+    print("Please enter the file from the test folder that you wish to run or type exit() to exit the program: ")
+    file_path = input("file: ")
+    if file_path == "exit()":
+        print("EVENT: Exiting program...")
+        exit(0)
+    root_path = os.path.dirname(os.path.abspath(__file__))
+    full_path = Path(root_path + "/test/" + file_path)
+    if not full_path.is_file():
+        print_error("FILE PATH", "main()",
+            "The file name provided is not a valid file")
+        print("Please enter a valid file from the test folder\n")
+        main()
     print("EVENT: Running main...")
-    parsedDfa = parse_main()
+    parsedDfa = parse_main(full_path)
     print("EVENT: DFA PARSE RESULTS")
     print(parsedDfa.states)
     print(parsedDfa.alpha)
@@ -18,23 +35,15 @@ def main():
     print("EVENT: Verifying DFA...")
     result = verify_dfa_main(parsedDfa)
     if result:
+        dfa_min = minimize_main(parsedDfa)
+        if dfa_min is None:
+            print_error("MINIMIZE", "main()", "DFA cannot be minimized!!")
+            print("EVENT: Finished Main.....")
+            return exit(1)
         pretty_print_main(parsedDfa)
-        print("EVENT: Finished Main.....")
     else:
-        print("VERIFY ERROR: DFA cannot be verified!!")
+        print_error("VERIFY", "main()", "DFA cannot be verified!!")
+    print("EVENT: Finished Main.....")
+    return exit(0)
 
-
-def main2():
-    print("EVENT: Running main2...")
-    parsedDfa = parse_main()
-    print("EVENT: DFA PARSE RESULTS")
-    print(parsedDfa.states)
-    print(parsedDfa.alpha)
-    print(parsedDfa.transfunc)
-    print(parsedDfa.start)
-    print(parsedDfa.final)
-    print("EVENT: Converting DFA to set of State Objects...")
-    minimize_main(parsedDfa)
-
-
-main2()
+main()
--- src/verify.py
+++ src/verify.py
@@ -1,73 +1,124 @@
 import src
 from src.dfa import Dfa
+from src.error import print_error
 
 
 def verify_dfa_main(dfa):
     results = []
-    results.append(verify_states(dfa.states))
+    results.append(verify_states(dfa.states, dfa.start, dfa.final))
     results.append(verify_alpha(dfa.alpha))
     results.append(verify_trans(dfa.transfunc, dfa.start,
                                 dfa.final, dfa.states, dfa.alpha))
     results.append(verify_start(dfa.start, dfa.states))
     results.append(verify_final(dfa.final, dfa.states))
     if False in results:
+        print_error("VERIFY", "verify_dfa_main()", "Parsed DFA did not meet verification conditions")
         return False
     else:
         return True
 
 
-def verify_states(states):
-    return True
-
+def verify_states(states, start, final):
+    #At least 1
+    # Verify that there are only alphanumeric characters
+    # Verify that there are no duplicate states
+    # Verify that states are a superset of start and final
+    if (
+        len(states) >= 1 and
+        all(e.isalnum() for e in states) and
+        len(states) == len(set(states)) and 
+        set(states).issuperset(start) and 
+        set(states).issuperset(final)
+    ):
+        return True
+    else:
+        print_error("VERIFY", "verify_states", "states do not meet the correct conditions")
+        return False
 
 def verify_alpha(alpha):
-    if len(alpha) > 1:
-        return False
-    else:
+    # At least one
+    # Verify that alpha is alphanumeric
+    # No duplicates
+    if ( 
+        len(alpha) >= 1 and
+        all(e.isalnum() for e in alpha) and 
+        len(alpha) == len(set(alpha))
+    ):
         return True
+    else:
+        print_error("VERIFY", "verify_alpha", "alphas do not meet the correct conditions")
+        return False
 
 
 def verify_trans(trans, start, final, states, alpha):
-    result = False
-    # Compare trans to make sure they are in the states
-    # Also compare trans to make sure that alphabet symbols are contained in alphabet
-    for lst in trans:
-        # Check if start state comes first
-        for items in lst[0]:
-            if start != items[0]:
-                return False
-        # Check if final state comes last
-        for items in lst[-1]:
-            if final != items[-1]:
-                return False
-        for items in lst:
-            if len(items) == 3:
-                if items[0] not in states or items[-1] not in states or items[1] not in alpha:
-                    return False
-                else:
-                    result = True
-            else:
-                return False
-
-    return result
+    #Every transition head is a subset of the states
+    #Every alphabet is in the alpha set
+    #Every transition output is a member of the set of states
+    #For each alphabet letter for a state there can only be one output per letter.
+    #Start and final states need to exist 
+    start_check = False
+    final_check = False
+    for items in trans:
+        first = items[0]
+        alph = items[1]
+        last = items[-1]
+        if (
+            set(first).issubset(states) and 
+            set(alph).issubset(alpha) and
+            set(last).issubset(states)
+        ):
+            if set(first).issubset(first):
+                start_check = True
+            if set(last).issubset(final):
+                final_check = True
+        else:
+            print_error("VERIFY", "verify_trans", "transitions do not meet the correct conditions")
+            return False
+    
+    for state in states:
+        for als in alpha:
+            match = [a for a in trans if state in a[0] and als in a[1]]
+        if len(match) == 1 and start_check and final_check:
+            return True
+        else:
+            print_error("VERIFY", "verify_trans", "transitions do not meet the correct conditions")
+            return False
 
 
 def verify_start(start, states):
-    if len(start) > 1 or start not in states:
-        return False
-    else:
+    # Verify that there is only one start state
+    # Start state needs to be a subset of states
+    # Start should be alphanumeric
+    if (
+        len(start) == 1 and
+        start.isalnum() and 
+        set(start).issubset(states)
+    ):
         return True
+    else:
+        print_error("VERIFY", "verify_start", "start does not meet the correct conditions")
+        return False
 
 
 def verify_final(final, states):
-    if len(final) > 1 or final not in states:
-        return False
-    else:
+    #Must not be empty
+    #Alphanumeric
+    #Final states must have no duplicates
+    #Final states are a subset of states
+    if (
+        len(final) >= 1 and 
+        all(e.isalnum() for e in final) and
+        len(final) == len(set(final)) and 
+        set(final).issubset(states)
+    ):
         return True
+    else:
+        print_error("VERIFY", "verify_final", "final does not meet the correct conditions")
+        return False
 
 
 def verify_state_objects_main(state_list):
-    start_state = [s for s in state_list if s.is_start]
+    start_state=[s for s in state_list if s.is_start]
     if start_state == 1:
         return True
     return False
